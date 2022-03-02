228abc51b1d5674316806f0dfdcff275fcb5f4d4
Cristian Ordonez <cristianordonezrd@gmail.com>
2022-03-01 20:39:47 -0500

    completed adding linethrough on toggle

--- src/todoitem.js
+++ src/todoitem.js
@@ -4,22 +4,26 @@ import ListItemText from "@mui/material/ListItemText";
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
-// import IconButton from '@mui/icons-material/IconButton';
 import ToggleState from './hooks/ToggleState.js'
-
+import Typography from '@mui/material/Typography';
 
 function TodoItem({task, completed}) {
   console.log(task)
   const [isOn, setIsOn] = ToggleState(completed);
 
+
   return (
-    <ListItem>
+    // <Typography>
+
+    <ListItem >
       <Checkbox checked={completed} onChange={e => {setIsOn()}}/>
-      <ListItemText>{task}</ListItemText>
+      <ListItemText style={{textDecoration: isOn ? 'line-through' : 'none'}}
+      >{task}</ListItemText>
       <IconButton>
         <DeleteIcon />
       </IconButton>
     </ListItem>
+    // </Typography>
   )
 }
 
